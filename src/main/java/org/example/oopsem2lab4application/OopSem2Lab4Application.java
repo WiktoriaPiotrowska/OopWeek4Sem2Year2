d87664286ae797ce5d452c2afc039eb0f7290c14
wiktoria <G00466677@atu.ie>
2026-02-22 21:26:45 +0000

    First Commit

---
+++ src/main/java/org/example/oopsem2lab4application/OopSem2Lab4Application.java
@@ -0,0 +1,13 @@
+package org.example.oopsem2lab4application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSem2Lab4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopSem2Lab4Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/oopsem2lab4application/OopSem2Lab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.oopsem2lab4application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSem2Lab4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
